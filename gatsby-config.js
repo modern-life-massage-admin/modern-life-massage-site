187486154567e0ce6a2051117d83dfabb2fde490
Dean Nellis <nellisdean@gmail.com>
2021-08-17 21:43:09 -0400

    Added Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
