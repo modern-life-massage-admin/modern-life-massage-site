fce327ada62f5a6fb3e5b42103d95f1a5c9002e7
Dean Nellis <nellisdean@gmail.com>
2021-08-23 00:44:23 -0400

    Sourced content

--- src/components/HoursSection/index.tsx
+++ src/components/HoursSection/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
 import AboutPageData from "../../../content/aboutpage.json";
+import FooterData from "../../../content/footercontent.json";
 
 const HoursSection = () => {
   const { hours } = AboutPageData;
+  const { address1, address2, address3, phoneNumber } = FooterData;
   return (
     <section className="hours-section">
       <div className="hours-section__content">
@@ -22,11 +24,14 @@ const HoursSection = () => {
         <div className="hours-section__address">
           <h3>Address</h3>
           <p>
-            1716 Sharkey Way
+            {address1}
             <br />
-            Suite 200
+            {address2}
             <br />
-            Lexington, KY 40511-2096
+            {address3}
+          </p>
+          <p>
+            <strong>{phoneNumber}</strong>
           </p>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3141.114727273424!2d-84.53042838467388!3d38.06770917970724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88424383f0f00001%3A0x869b174393c13b24!2s1716%20Sharkey%20Way%20Suite%20200%2C%20Lexington%2C%20KY%2040511!5e0!3m2!1sen!2sus!4v1628224904589!5m2!1sen!2sus"
