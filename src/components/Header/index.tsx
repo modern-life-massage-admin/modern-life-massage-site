de94c32af54f7f0f63d6f0e017e7185c56b53f4e
modern-life-massage-admin <jeff@modernlifemassagetherapy.com>
2021-10-04 23:09:23 -0400

    Updated mobile header and team portrait

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -42,32 +42,43 @@ const Header = () => {
               </li>
             </ul>
           </nav>
+          <div className="header__cta--desktop">
+            <Link to="/book-now">
+              <Button onClick={() => {}} variant="primary">
+                Book Now
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="header__mobile-nav">
+        <div className="header__cta--mobile">
           <Link to="/book-now">
             <Button onClick={() => {}} variant="primary">
               Book Now
             </Button>
           </Link>
         </div>
-      </div>
-      <div className="header__menu-button">
-        <Button
-          variant="no-style"
-          onClick={() => {
-            setMenuIsOpen(!menuIsOpen);
-          }}
-        >
-          <svg
-            width="32"
-            height="32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="header__menu-button">
+          <Button
+            variant="no-style"
+            onClick={() => {
+              setMenuIsOpen(!menuIsOpen);
+            }}
           >
-            <path
-              d="M29.118 6H2.882A.882.882 0 002 6.882v2.236c0 .487.395.882.882.882h26.236A.882.882 0 0030 9.118V6.882A.882.882 0 0029.118 6zM29.118 14H2.882a.882.882 0 00-.882.882v2.236c0 .487.395.882.882.882h26.236a.882.882 0 00.882-.882v-2.236a.882.882 0 00-.882-.882zM29.118 22H2.882a.882.882 0 00-.882.882v2.236c0 .487.395.882.882.882h26.236a.882.882 0 00.882-.882v-2.236a.882.882 0 00-.882-.882z"
-              fill="#fff"
-            />
-          </svg>
-        </Button>
+            <svg
+              width="32"
+              height="32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M29.118 6H2.882A.882.882 0 002 6.882v2.236c0 .487.395.882.882.882h26.236A.882.882 0 0030 9.118V6.882A.882.882 0 0029.118 6zM29.118 14H2.882a.882.882 0 00-.882.882v2.236c0 .487.395.882.882.882h26.236a.882.882 0 00.882-.882v-2.236a.882.882 0 00-.882-.882zM29.118 22H2.882a.882.882 0 00-.882.882v2.236c0 .487.395.882.882.882h26.236a.882.882 0 00.882-.882v-2.236a.882.882 0 00-.882-.882z"
+                fill="#fff"
+              />
+            </svg>
+          </Button>
+        </div>
       </div>
     </header>
   );
